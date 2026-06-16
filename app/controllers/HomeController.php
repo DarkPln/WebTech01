a8190241570d1033c502693f43aafa23c82f92e0
nic16 <100148073+nic16@users.noreply.github.com>
2026-06-16 14:53:43 +0200

    Comment_HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,9 +1,12 @@
 <?php
-
+//Niclas: Carousel
 class HomeController extends Controller {
     public function index(): void {
+        //holt Fahrzeuge aus DB
         $carouselCars   = Car::getForCarousel();
+        //Wie oft muss Carousel wiederholt werden, damit immer 18 Fahrzeuge angezeigt werden? (mindestens 3 Wiederholungen, damit es nicht zu leer aussieht)
         $carouselRepeat = empty($carouselCars) ? 0 : max(3, (int)ceil(18 / count($carouselCars)));
         $this->render('home/index', compact('carouselCars', 'carouselRepeat'));
+        //compact() erstellt ein Array aus den übergebenen Variablen, damit sie in der View (home/index.php) verfügbar sind    
     }
 }
